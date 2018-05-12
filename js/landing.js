7e8496ee99514fda234fda69deb3e81bb34b8d47
Shitov Dmitry <shitov.dm@gmail.com>
2018-05-12 03:27:27 +0300

    Completely refined main slider.

--- js/landing.js
+++ js/landing.js
@@ -1,32 +1,71 @@
 
+/**
+ * Simple slider for landing page.
+ * Can work in Google Chrome extension.
+ * @author Shitov Dmitry 2018
+ */
+
+var currentSlide = 0;   //  Number of current slide.
+var limitSlide = 2; //  Amount of slides(half withput null).
+
 $(document).ready(function() {
     //  Welcome page slider.
-    $(".right-shift").on("click",function(){
-        slider__changePage("forth");
+    $(".right-arrow").on("click",function(){
+        _changeSlide("forth");
     });
-    $(".left-shift").on("click",function(){
-        slider__changePage("back");
+    $(".left-arrow").on("click",function(){
+        _changeSlide("back");
     });
-    
-    
-    
 });
 
 
-
-function slider__changePage(action){
-    console.log(action);
+/**
+ * Function calculated number of current slide and motion direction.
+ * @param {type} action
+ * @returns {undefined}
+ */
+function _changeSlide(action){
     if(action === "forth"){
-        //  Go to back page.
-        $(".welcome-page").css("display","none");
+        //  Forward;
+        if(currentSlide < limitSlide){
+            currentSlide++;
+            toSlide(currentSlide,"forth");
+        }
+        (currentSlide === limitSlide) ? ($(".right-arrow").fadeOut(600)) : ($(".left-arrow").fadeIn(600));
     }else{
         if(action === "back"){
-             //  Go to next page.
-             $(".features-page").fadeIn(50);
-             $('.features-page').animate({'left':'0'},400) ;    
-             $('.welcome-page').animate({'left':'100%'},400) ; 
+            //  Go to back page.
+            if(currentSlide > -limitSlide){
+                currentSlide--;
+                toSlide(currentSlide,"back");
+            }
+            (currentSlide === -limitSlide) ? ($(".left-arrow").fadeOut(600)) : ($(".right-arrow").fadeIn(600));
         }
+    }  
+}
+
+/**
+ * Function hide/show determinated slide.
+ * @param {Integer} slideNumber     This slide was removed.
+ * @param {String} direction    Direction of motion.
+ * @returns {undefined}
+ * 
+ */
+function toSlide(slideNumber,direction) {
+    var lastSlideClassName = "";
+    var lastSlidePos = 0;
+    
+    if(direction === "forth"){
+        lastSlideClassName = "slide-" + (slideNumber - 1);
+        lastSlidePos = -100 + "%";
+    }else{
+        if(direction === "back"){
+            lastSlideClassName = "slide-" + (slideNumber + 1);
+            lastSlidePos = 100 + "%";
+        } 
     }
     
-    
+    $(".slide-" + slideNumber).fadeIn(50);
+    $(".slide-" + slideNumber).animate({'left': "0%"}, 400);
+    $("." + lastSlideClassName).animate({'left': lastSlidePos}, 400);
 }
